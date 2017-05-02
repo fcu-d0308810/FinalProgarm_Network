38ead7c1fceb23c5ecc8435c898efe3daf92dc32
Lo 4J <ihaveadream0528@yahoo.com.tw>
2017-05-02 15:35:19 +0800

    正在試將googlemap加入

--- app/src/main/java/com/example/ihaveadream0528/finalprogarm/GoogleMap_fragment.java
+++ app/src/main/java/com/example/ihaveadream0528/finalprogarm/GoogleMap_fragment.java
@@ -1,21 +1,26 @@
 package com.example.ihaveadream0528.finalprogarm;
 
-import android.content.Context;
-import android.net.Uri;
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 
 public class GoogleMap_fragment extends Fragment {
+    private MapView mapView;
+    private GoogleMap map;
+
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
         return initView(inflater, container);
     }
 
     private View initView(LayoutInflater inflater, ViewGroup container) {
         View view = inflater.inflate(R.layout.google_map_fragment, container, false);
+        mapView = (MapView) view.findViewById(R.id.map);
+
         return view;
     }
 }
