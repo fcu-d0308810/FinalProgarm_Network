28ce70465c2e6194abe595b55daae668c7f7938c
Lo 4J <ihaveadream0528@yahoo.com.tw>
2017-09-19 12:37:20 +0800

    用來新增管理gridView中的imageView

---
+++ app/src/main/java/com/example/ihaveadream0528/finalprogarm/ImageAdapter.java
@@ -0,0 +1,47 @@
+package com.example.ihaveadream0528.finalprogarm;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+
+public class ImageAdapter extends BaseAdapter{
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        ImageView imageView;
+        if(view == null){
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        }
+        else{
+            imageView = (ImageView) view;
+        }
+        //imageView.setImageBitmap();
+        return imageView;
+    }
+}
