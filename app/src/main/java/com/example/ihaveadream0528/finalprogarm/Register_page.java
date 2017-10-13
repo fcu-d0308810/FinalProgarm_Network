334563344a8b1225a273734d6ac35f659d2c2f02
Lo 4J <ihaveadream0528@yahoo.com.tw>
2017-10-13 15:30:17 +0800

    與前端註冊介面綁定物件

--- app/src/main/java/com/example/ihaveadream0528/finalprogarm/Register_page.java
+++ app/src/main/java/com/example/ihaveadream0528/finalprogarm/Register_page.java
@@ -8,21 +8,28 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Register_page extends AppCompatActivity {
-    private EditText username_edittext, password_edittext, invitationCode_edittext;
+    private EditText username_edittext, password_edittext, invitationCode_edittext, userID_edittext
+            ,checkPassword_edittext;
     private Button confirm_button, cancel_button;
+    private DatabaseReference databaseReference, user_databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_page);
-        username_edittext = (EditText)findViewById(R.id.username_edittext);
-        password_edittext = (EditText)findViewById(R.id.password_edittext);
-        invitationCode_edittext = (EditText)findViewById(R.id.invitationCode_edittext);
-        confirm_button = (Button)findViewById(R.id.confirm_button);
+        username_edittext = (EditText) findViewById(R.id.username_edittext);
+        password_edittext = (EditText) findViewById(R.id.password_edittext);
+        invitationCode_edittext = (EditText) findViewById(R.id.invitationCode_edittext);
+        checkPassword_edittext  = (EditText) findViewById(R.id.check_password_edittext);
+        confirm_button = (Button) findViewById(R.id.confirm_button);
+        userID_edittext = (EditText) findViewById(R.id.userid_edittext);
         confirm_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                checkEmpty();
             }
         });
         cancel_button = (Button)findViewById(R.id.cancel_button);
@@ -33,5 +40,38 @@ public class Register_page extends AppCompatActivity {
                 startActivity(new Intent(Register_page.this, Login_page.class));
             }
         });
+    }
+    private void checkEmpty(){
+        // check ID isn't empty
+        if(!userID_edittext.getText().equals("")){
+            // check password isn't empty
+            if(!password_edittext.getText().equals("")){
+                // check password is same with check password
+                if(password_edittext.getText().equals(checkPassword_edittext.getText())){
+                    // check username isn't empty
+                    if(!username_edittext.getText().equals("")){
+                        // check invitation code isn't empty
+                        if(!invitationCode_edittext.getText().equals("")){
+                            databaseReference = FirebaseDatabase.getInstance().getReference().child("TEST0919");
+                        }
+                        else{
+
+                        }
+                    }
+                    else{
+
+                    }
+                }
+                else{
+
+                }
+            }
+            else{
+
+            }
+        }
+        else{
+
+        }
     }
 }
